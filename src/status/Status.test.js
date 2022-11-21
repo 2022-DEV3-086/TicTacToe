c4d56751b3a0dcdd9bdfef4b28e254592cc9049b
Harikrishnan <118582559+2022-DEV3-086@users.noreply.github.com>
2022-11-21 18:07:10 +0530

    Refactor: group similar tasks in a test file.

--- src/status/Status.test.js
+++ src/status/Status.test.js
@@ -3,17 +3,23 @@ import { TestConstants } from "../constants/TestConstants";
 import Status from "./Status";
 
 describe("Status component", () => {
-  test("should display correct message if player 2 is going to play next", () => {
-    const { PLAYER2, PLAYER2_TITLE, MARK_REQUEST_MESSAGE } = TestConstants;
-    render(<Status player={PLAYER2} />);
+  const testStatusMessageOfThePlayer = (player) => {
+    const { PLAYER1, PLAYER1_TITLE, PLAYER2_TITLE, MARK_REQUEST_MESSAGE } =
+      TestConstants;
+    render(<Status player={player} />);
     const status = screen.getByTestId("status");
-    expect(status.textContent).toBe(`${PLAYER2_TITLE} ${MARK_REQUEST_MESSAGE}`);
+    expect(status.textContent).toBe(
+      `${
+        player === PLAYER1 ? PLAYER1_TITLE : PLAYER2_TITLE
+      } ${MARK_REQUEST_MESSAGE}`
+    );
+  };
+
+  test("should display correct message if player 2 is going to play next", () => {
+    testStatusMessageOfThePlayer(TestConstants.PLAYER1);
   });
 
   test("should display correct message if player 1 is going to play next", () => {
-    const { PLAYER1, PLAYER1_TITLE, MARK_REQUEST_MESSAGE } = TestConstants;
-    render(<Status player={PLAYER1} />);
-    const status = screen.getByTestId("status");
-    expect(status.textContent).toBe(`${PLAYER1_TITLE} ${MARK_REQUEST_MESSAGE}`);
+    testStatusMessageOfThePlayer(TestConstants.PLAYER2);
   });
 });
