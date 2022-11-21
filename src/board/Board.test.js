9dd99e03648b9a38c1ade2534553f7b46e89ae0b
Harikrishnan <118582559+2022-DEV3-086@users.noreply.github.com>
2022-11-21 12:28:36 +0530

    Refactor: group similar tasks in test files.

--- src/board/Board.test.js
+++ src/board/Board.test.js
@@ -3,7 +3,8 @@ import { TestConstants } from "../constants/TestConstants";
 import Board from "./Board";
 
 describe("Board component", () => {
-  test("should have 3x3 tiles in it", () => {
+  let tiles;
+  beforeEach(() => {
     render(
       <Board
         tiles={Array(TestConstants.NUMBER_OF_TILES_ON_THE_BOARD).fill(
@@ -11,19 +12,14 @@ describe("Board component", () => {
         )}
       />
     );
-    const tiles = screen.queryAllByTestId("tile");
+    tiles = screen.queryAllByTestId("tile");
+  });
+
+  test("should have 3x3 tiles in it", () => {
     expect(tiles).toHaveLength(TestConstants.NUMBER_OF_TILES_ON_THE_BOARD);
   });
 
   test("tiles should have empty value as initial marking", () => {
-    render(
-      <Board
-        tiles={Array(TestConstants.NUMBER_OF_TILES_ON_THE_BOARD).fill(
-          TestConstants.EMPTY
-        )}
-      />
-    );
-    const tiles = screen.queryAllByTestId("tile");
     for (const tile of tiles) {
       expect(tile.textContent).toBe(TestConstants.EMPTY);
     }
--- src/tictactoe/TicTacToe.test.js
+++ src/tictactoe/TicTacToe.test.js
@@ -3,15 +3,17 @@ import { TestConstants } from "../constants/TestConstants";
 import TicTacToe from "./TicTacToe";
 
 describe("TicTacToe component", () => {
-  test("should display header with title", () => {
+  beforeEach(() => {
     render(<TicTacToe />);
+  });
+
+  test("should display header with title", () => {
     const header = screen.getByTestId("header");
     expect(header).toBeInTheDocument();
     expect(header.textContent).toBe(TestConstants.TITLE);
   });
 
   test("should display the board", () => {
-    render(<TicTacToe />);
     const board = screen.getByTestId("board");
     expect(board).toBeInTheDocument();
   });
